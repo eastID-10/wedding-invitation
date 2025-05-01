0a4bf3900ea2fdb9636b0a33d126a1b36fc54710
eastID-10 <eastidmyt@gmail.com>
2025-05-01 20:59:43 +0700

    polished

---
+++ assets/js/scrollSmooth.js
@@ -0,0 +1,11 @@
+const lenis = new Lenis({
+    duration: 1.5,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)) // ease-out
+  });
+  
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+  
