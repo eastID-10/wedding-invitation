5d13a203b3f9021a8ae320ae085a496f8f483e25
aji <azhikun13@gmail.com>
2025-05-02 14:13:38 +0700

    fix music

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const audio = document.getElementById('bgAudio');
+
+    document.getElementById('openInvitation').addEventListener('click', () => {
+      console.log('audio clicked');
+      audio.volume = 0.5; // 50% volume
     
-    // Set initial volume (0.0 to 1.0, where 1.0 is full volume)
-    audio.volume = 0.5; // 50% volume
-    
-    // Most browsers require muted autoplay initially
-    audio.muted = false;
-    
-    audio.play().catch(e => {
-      console.log("Autoplay was prevented:", e);
+      // Most browsers require muted autoplay initially
+      audio.muted = false;
+      
+      audio.play().catch(e => {
+        console.log("Autoplay was prevented:", e);
+      });
     });
   });
